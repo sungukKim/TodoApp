53d46565e375e1e3416e58cf5d2eeccb2336bc96
sung uk <sunguk0204@monkey-soft.com>
2022-02-28 19:18:11 +0900

    Add Todolist,item

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import DateHead from './components/DateHead';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
+import Todolist from './components/Todolist';
 
 function App() {
   const today = new Date();
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
 
   return (
     <SafeAreaProvider>
@@ -15,7 +22,7 @@ function App() {
           behavior={Platform.select({ios: 'padding'})}
           style={styles.avoid}>
           <DateHead date={today} />
-          <Empty />
+          {todos.length === 0 ? <Empty /> : <Todolist todos={todos} />}
           <AddTodo />
         </KeyboardAvoidingView>
       </SafeAreaView>
